61d5a4dd457b32554578ff5a04cc9597ab3b5bcf
Greg Burri <greg.burri@gmail.com>
2014-10-22 13:26:29 +0200

    Add a scene to test terrain features.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+   public GameObject player;
+   Vector3 offset;
+   
+   // Use this for initialization
+   void Start()
+   {
+      this.offset = this.transform.position - this.player.transform.position;
+   }
+   
+   // Update is called once per frame
+   void LateUpdate()
+   {
+      this.transform.position = this.player.transform.position + this.offset;
+   }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+   /*void Update()
+   {
+   }*/
+   
+   public float speed;
+   int count;
+   
+   void Start()
+   {
+   }
+
+   void FixedUpdate()
+   {
+      float moveHorizontal = Input.GetAxis("Horizontal");
+      float moveVertical = Input.GetAxis("Vertical");
+      
+      var movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+      this.rigidbody.AddForce(movement * this.speed * Time.deltaTime);
+   }
+}
+
