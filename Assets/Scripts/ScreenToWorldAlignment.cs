31733f0266c16ce10306b401069d427b7af188c3
ice-blaze <franketip@gmail.com>
2014-10-31 16:48:51 +0100

    Center the menu with a new script
    It take the main camera position to know where to put the buttons.
    Warning, we can only have 3 buttons for now

---
+++ Assets/Scripts/ScreenToWorldAlignment.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class ScreenToWorldAlignment : MonoBehaviour 
+{
+	public enum Index {First, Second, Third};
+	public Index index; 
+
+	void Update () 
+	{
+		Vector3 tempScreenPosition = new Vector3 (0, 0, -Camera.main.transform.position.z);
+		
+		Vector3 worldPosition = Camera.main.ScreenToWorldPoint(tempScreenPosition);
+		worldPosition.x -= renderer.bounds.size.x;// * tempScreenPosition.x / Screen.width;
+		worldPosition.y += renderer.bounds.size.y;// * (1 - tempScreenPosition.y / Screen.height);
+		worldPosition.x *= 0.5f;
+		worldPosition.y *= 0.5f;
+
+		if (index == Index.First)
+        {
+            worldPosition.y += renderer.bounds.size.y;
+        } else if (index == Index.Third)
+        {
+            worldPosition.y -= renderer.bounds.size.y;
+        }
+
+		worldPosition.z += 5*renderer.bounds.size.y;
+		transform.position = worldPosition;
+	}
+}
