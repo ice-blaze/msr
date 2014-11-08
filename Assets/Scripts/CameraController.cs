ab75cd3a79320058744ded5600ed08561c2d44c6
nathanquinteiro <nathan.quinteiro@gmail.com>
2014-11-08 19:48:45 +0100

    Add a scene with the Rover and modified scripts

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,18 +3,54 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour
 {
-   public GameObject player;
-   Vector3 offset;
+	public Transform car;
+	public float distance;
+	public float height;
+	public float rotationDamping;
+	public float heightDamping;
+	public float zoomRatio;
+	public float defaultFOV;
+	private Vector3 rotationVector;
+   //	public GameObject player;
+   	//Vector3 offset;
+	//Quaternion offsetangle;
    
-   // Use this for initialization
-   void Start()
-   {
-      this.offset = this.transform.position - this.player.transform.position;
-   }
+   	// Use this for initialization
+   	void Start()
+   	{
+   	}
    
-   // Update is called once per frame
-   void LateUpdate()
-   {
-      this.transform.position = this.player.transform.position + this.offset;
-   }
+   	// Update is called once per frame
+   	void LateUpdate()
+   	{
+		float wantedAngle = rotationVector.y;
+		float wantedHeight = car.position.y + height;
+		float myAngle = transform.eulerAngles.y;
+		float myHeight = transform.position.y;
+		myAngle = Mathf.LerpAngle(myAngle,wantedAngle,rotationDamping*Time.deltaTime);
+		myHeight = Mathf.Lerp(myHeight,wantedHeight,heightDamping*Time.deltaTime);
+		Quaternion currentRotation = Quaternion.Euler(0,myAngle,0);
+		transform.position = car.position;
+		transform.position -= currentRotation*Vector3.forward*distance;
+		transform.position += Vector3.up * (myHeight-transform.position.y);
+		transform.LookAt(car);
+
+
+     // 	this.transform.position = this.player.transform.position + this.offset;
+	//	this.transform.rotation.Set(this.transform.rotation.x,this.transform.rotation.y,this.offsetangle.z,this.transform.rotation.w);
+   	}
+
+	void FixedUpdate ()
+	{
+		Vector3 localVilocity = car.InverseTransformDirection(car.rigidbody.velocity);
+		if (localVilocity.z < -0.1){
+			rotationVector.y = car.eulerAngles.y + 180;
+		}
+		else {
+			rotationVector.y = car.eulerAngles.y;
+		}
+		var acc = car.rigidbody.velocity.magnitude;
+		camera.fieldOfView = defaultFOV + acc*zoomRatio;
+	}
 }
+
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,24 +3,47 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
+	public WheelCollider WheelLF;
+	public WheelCollider WheelLB;
+	public WheelCollider WheelRF;
+	public WheelCollider WheelRB;
+	public Transform WheelLFTransform;
+	public Transform WheelLBTransform;
+	public Transform WheelRFTransform;
+	public Transform WheelRBTransform;
+	int maxTorque = 300;
    /*void Update()
    {
    }*/
    
-   public float speed;
-   int count;
-   
    void Start()
    {
+
+		rigidbody.centerOfMass = Vector3.down * 1f;
    }
 
    void FixedUpdate()
    {
-      float moveHorizontal = Input.GetAxis("Horizontal");
-      float moveVertical = Input.GetAxis("Vertical");
-      
-      var movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-      this.rigidbody.AddForce(movement * this.speed * Time.deltaTime);
+		WheelLF.motorTorque = maxTorque * Input.GetAxis("Vertical");
+		WheelRF.motorTorque = maxTorque * Input.GetAxis("Vertical");
+		WheelLF.steerAngle = 20 * Input.GetAxis("Horizontal");
+		WheelRF.steerAngle = 20 * Input.GetAxis("Horizontal");
+
+	
+       
+//      var movement = new Vector3(moveVertical, 0, -moveHorizontal);
+//      this.rigidbody.AddForce(movement * this.speed * Time.deltaTime);
    }
+	void Update()
+	{
+		WheelLFTransform.Rotate (0,WheelLF.rpm / 60 * -360 * Time.deltaTime,0);
+		WheelRFTransform.Rotate (0,WheelLF.rpm / 60 * -360 * Time.deltaTime,0);
+		WheelLBTransform.Rotate (0,WheelLB.rpm / 60 * -360 * Time.deltaTime,0);
+		WheelRBTransform.Rotate (0,WheelLB.rpm / 60 * -360 * Time.deltaTime,0);
+
+		WheelLFTransform.localEulerAngles += Vector3.up * (WheelLF.steerAngle - WheelLFTransform.localEulerAngles.z - 180 - WheelLFTransform.localEulerAngles.y);
+		WheelRFTransform.localEulerAngles += Vector3.up * (WheelRF.steerAngle - WheelRFTransform.localEulerAngles.z - 180 - WheelRFTransform.localEulerAngles.y);
+
+	}
 }
 
