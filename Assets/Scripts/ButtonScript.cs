e7cb673e7a4360c4c6110d30c2894506663d23c1
ice-blaze <franketip@gmail.com>
2014-11-17 22:57:01 +0100

    Menu with planet, new fonts, levels, about and animation transision

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -6,51 +6,58 @@ using System.Collections.Generic;
 
 [RequireComponent(typeof(Renderer))]
 public class ButtonScript : MonoBehaviour {
-	
+	public enum Role{newgame,about,exit};
+	public Role role;
 	public GameObject selectors;
 	public bool isSelected;
 	static public GameObject selected = null; 
 	public GameObject nextButton;
 	public GameObject prevButton;
-	public int levelIndex = 0;
 	public bool isExit = false;
-	
+	InputControllerScript inputController;
+
 	private void Start(){
 		// next button et prev can't be empty
-		if (nextButton == null || prevButton == null) {
+		if (nextButton == null || prevButton == null || role==null) {
 			Debug.LogError("member nextButton or prevButton can't be null");
 		}
 		if (isSelected == true && selected == null) {
 			selected = gameObject;
 			select ();
 		}
+		inputController = GameObject.Find("InputController").GetComponent<InputControllerScript>();
+	}
+
+	public static void selectActual(){
+		selected.GetComponent<ButtonScript>().select();
 	}
 	
 	public void select(){
 		isSelected = true;
-		renderer.material.color = Color.yellow;
 		selectors.renderer.enabled = true;
 	}
 	
 	public void deselect(){
 		isSelected = false;
-		renderer.material.color = Color.white;
 		selectors.renderer.enabled = false;
 	}
 	
 	public void execute(){
-		if (isExit) {
-			Application.Quit();
-			//UnityEditor.EditorApplication.isPlaying = false;
-		}
-		Application.LoadLevel(levelIndex);
+		switch(role){
+		case Role.newgame:
+			inputController.quitStartMenu();
+			InputControllerScript.actualMenu = InputControllerScript.Menu.LevelSelection;
+//			inputController.startLevelMenu();
+			break;
+		case Role.about:
+			inputController.quitStartMenu();
+			InputControllerScript.actualMenu = InputControllerScript.Menu.About;
+			break;
+		case Role.exit:
+			InputControllerScript.applicationQuit();
+			break;
+		default:
+			break;
+		}			
 	}
-	
-	//	void OnMouseEnter() {
-	//		select ();
-	//	}
-	//	
-	//	void OnMouseExit(){
-	//		deselect ();
-	//	}
 }
--- Assets/Scripts/InputControllerScript.cs
+++ Assets/Scripts/InputControllerScript.cs
@@ -5,22 +5,72 @@ using System.Collections;
 
 public class InputControllerScript : MonoBehaviour {
 
-	// Use this for initialization
+	public enum Menu{Start,LevelSelection,About};
+	public static Menu actualMenu = Menu.Start;
+
+	GameObject startMenu;
+	Animator startAnim;
+	GameObject levelMenu;
+	Animator levelAnim;
+	GameObject aboutMenu;
+	Animator aboutAnim;
+
+	public bool isAnimated = true;
+
+	LevelMenuScript levelScript;
+	AboutMenuScript aboutScript;
+
 	void Start () {
-	
+		startMenu = GameObject.Find("StartMenu");
+		levelMenu = GameObject.Find("LevelMenu");
+		aboutMenu = GameObject.Find("AboutMenu");
+
+		startAnim = startMenu.GetComponent<Animator>();
+		levelAnim = levelMenu.GetComponent<Animator>();
+		aboutAnim = aboutMenu.GetComponent<Animator>();
+
+		levelScript = levelMenu.GetComponent<LevelMenuScript>();
+		aboutScript = aboutMenu.GetComponent<AboutMenuScript>();
 	}
 
-	// Update is called once per frame
 	void Update () {
+		if(isAnimated){return;}
+
+
 		if (Input.GetKeyDown (KeyCode.DownArrow)) {
 			selectNextButton ();
 		} else if (Input.GetKeyDown (KeyCode.UpArrow)) {
 			selectPrevButton ();
 		} else if (Input.GetKeyDown (KeyCode.Return)) {
-			getSelected ().execute ();
+			isAnimated = true;
+			switch(actualMenu){
+			case Menu.Start:
+				getSelected().execute();
+				break;
+			case Menu.LevelSelection:
+				levelScript.execute();
+				break;
+			case Menu.About:
+				aboutScript.quit();
+				break;
+			default:
+				break;
+			}
 		} else if (Input.GetKeyDown (KeyCode.Escape)) {
-			Application.Quit();
-		//	UnityEditor.EditorApplication.isPlaying = false;
+			isAnimated = true;
+			switch(actualMenu){
+			case Menu.LevelSelection:
+				levelScript.quit();
+				break;
+			case Menu.About:
+				aboutScript.quit();
+				break;
+			case Menu.Start:
+				applicationQuit();
+				break;
+			default:
+				break;
+			}
 		}
 	}
 
@@ -29,14 +79,112 @@ public class InputControllerScript : MonoBehaviour {
 	}
 
 	private void selectNextButton(){
-		getSelected().deselect();
-		ButtonScript.selected = getSelected().nextButton;
-		getSelected().select ();
+		switch(actualMenu){
+			case Menu.Start:
+				getSelected().deselect();
+				ButtonScript.selected = getSelected().nextButton;
+				getSelected().select ();
+				break;
+			case Menu.LevelSelection:
+				isAnimated=true;
+				levelScript.nextButton();
+				break;
+			default:
+				break;
+		}
 	}
 
 	private void selectPrevButton(){
-		getSelected().deselect();
-		ButtonScript.selected = getSelected().prevButton;
-		getSelected().select ();
+		switch(actualMenu){
+			case Menu.Start:
+				getSelected().deselect();
+				ButtonScript.selected = getSelected().prevButton;
+				getSelected().select ();
+				break;
+			case Menu.LevelSelection:
+				isAnimated=true;
+				levelScript.previousButton();
+				break;
+			default:
+				break;
+		}
+	}
+
+	public void startLevelMenu(){
+		levelAnim.SetTrigger("levelin");
+		renderChild(levelMenu,true);
+	}
+
+	public void startStartMenu(){
+		startAnim.SetTrigger("menuin");
+		renderChild(startMenu,true);
+		foreach(GameObject cube in GameObject.FindGameObjectsWithTag("selector")){
+			cube.renderer.enabled = false;
+		}
+		ButtonScript.selectActual();
+	}
+
+	public void startAboutMenu(){
+		aboutAnim.SetTrigger("aboutin");
+		renderChild(aboutMenu,true);
+	}
+
+	public void quitLevelMenu(){
+		isAnimated=true;
+		levelAnim.SetTrigger("levelout");
+	}
+
+	public void quitedLevelMenu(){
+		renderChild(levelMenu,false);
+		actualMenu = Menu.Start;
+		launchNextMenu();
+	}
+
+	public void quitStartMenu(){
+		isAnimated=true;
+		startAnim.SetTrigger("menuout");
+	}
+
+	public void quitedStartMenu(){
+		renderChild(startMenu,false);
+		launchNextMenu();
+	}
+
+	public void quitAboutMenu(){
+		isAnimated=true;
+		startAnim.SetTrigger("aboutout");
+	}
+	
+	public void quitedAboutMenu(){
+		renderChild(aboutMenu,false);
+		actualMenu = Menu.Start;
+		launchNextMenu();
+	}
+
+	public void launchNextMenu(){
+		switch(actualMenu){
+			case Menu.Start:
+				startStartMenu();
+				break;
+			case Menu.LevelSelection:
+				startLevelMenu();
+				break;
+			case Menu.About:
+				startAboutMenu();
+				break;
+			default:
+				break;
+		}
+	}
+
+	public static void applicationQuit(){
+//		Application.Quit();
+		UnityEditor.EditorApplication.isPlaying = false;
+	}
+
+	public static void renderChild(GameObject parent,bool isEnabled){
+		foreach(Renderer child in parent.GetComponentsInChildren<Renderer>()){
+			child.renderer.enabled=isEnabled;
+		}
 	}
 }
