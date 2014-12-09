4ef0cbac7c54546962c6a0e187910c3a28fe6a4a
ice-blaze <franketip@gmail.com>
2014-12-09 17:02:40 +0100

    Change audio script

--- Assets/Scripts/SoundScript.cs
+++ Assets/Scripts/SoundScript.cs
@@ -3,26 +3,49 @@ using System.Collections;
 
 public class SoundScript : MonoBehaviour {
 
-	public AudioSource audioSource;
-	public float minPitch = 1.8f;
-	public float maxPitch = 2.5f;
+	AudioSource motorSource;
+	AudioSource voiceSource;
+	public float minMotorPitch = 0.0f;
+	public float maxMotorPitch = 4.5f;
+	public AudioClip motor;
+	public AudioClip hello;
+	public AudioClip alarm;
+	public AudioClip bonus;
 
 	// Use this for initialization
-	void Start () {
-		audioSource = GetComponent<AudioSource>();
-		audioSource.pitch = minPitch;
+	void Start () 
+	{
+//		audioSource = GetComponents<AudioSource>();
+		motorSource = gameObject.AddComponent<AudioSource>();
+		voiceSource = gameObject.AddComponent<AudioSource>();
+		motorSource.clip = motor;
+		motorSource.pitch = minMotorPitch;
+		motorSource.loop = true;
+		motorSource.playOnAwake = true;
+		motorSource.Play();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if(Input.GetAxis("Vertical")>0){
-			if(audioSource.pitch >= maxPitch) {return;}
-			audioSource.pitch += 0.1f;
-		} else if(audioSource.pitch>minPitch){
-			audioSource.pitch -= 0.1f;
-			if(audioSource.pitch < minPitch){
-				audioSource.pitch = minPitch;
+		if(Input.GetAxis("Vertical")>0||Input.GetAxis("Vertical")<0)
+		{
+			if(motorSource.pitch >= maxMotorPitch) 
+			{
+				return;
+			}
+			if(motorSource.pitch <= minMotorPitch)
+			{
+				motorSource.pitch = minMotorPitch;
+			}
+			motorSource.pitch += 0.1f;
+		} 
+		else if(motorSource.pitch>=minMotorPitch)
+		{
+			motorSource.pitch -= 0.1f;
+			if(motorSource.pitch <= minMotorPitch)
+			{
+				motorSource.pitch = 0.0f;
 			}
 		}
 	}
