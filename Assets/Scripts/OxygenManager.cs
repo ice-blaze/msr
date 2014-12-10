6249bf5006a4decbe0fb8c4c5dfcc6c4a97fdb17
ice-blaze <franketip@gmail.com>
2014-12-10 13:31:20 +0100

    Try new HUD

--- Assets/Scripts/OxygenManager.cs
+++ Assets/Scripts/OxygenManager.cs
@@ -14,7 +14,7 @@ public class OxygenManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Slider slider = gameObject.GetComponent<Slider>();
+		Slider slider = gameObject.GetComponentInChildren<Slider>();
 		slider.value = pc.oxygen;
 	}
 }
