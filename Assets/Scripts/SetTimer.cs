b18010c4ff0620d57083c8ab1e1a6d5f7c31a942
LastStandMatt <domus_nami@hotmail.fr>
2014-12-10 13:24:48 +0100

    Add Settimer scripts

---
+++ Assets/Scripts/SetTimer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SetTimer : MonoBehaviour {
+
+	//public GUIText ytext;
+	public float timerMax;
+	private int timerInt;
+	public bool decreases;
+	Text txt;
+	void Start () {
+		txt = gameObject.GetComponent<Text>(); 
+		txt.text = "" + System.Convert.ToString(timerMax);
+		if (decreases) { timerMax++;}
+	}
+	void Update()
+	{
+		if (decreases) 
+		{
+						if (timerMax > 0.1) {
+								
+								timerMax -= Time.deltaTime;
+								Debug.Log (timerMax);
+								timerInt = (int)timerMax;
+								txt.text = "" + System.Convert.ToString (timerInt);
+						}
+						//GameOver();
+		} 
+		else 
+		{
+			timerMax += Time.deltaTime;
+			Debug.Log (timerMax);
+			txt.text = "" + System.Convert.ToString (timerMax);
+		}
+	}
+}
